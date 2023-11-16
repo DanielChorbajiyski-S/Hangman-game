5706cd5e9fc60dd9c80653fc07aedc563ef8f689
DanielChorbajiyski <daniel.chorbajiyski.highschool@svetlina.net>
2023-11-16 17:57:20 +0200

    Added more code

--- Hangman/Hangman.cs
+++ Hangman/Hangman.cs
@@ -383,28 +383,59 @@
          @" __/══════╩═══"
         };
 
+        const char Underscore = '_';
 
         static void Main(string[] args)
         {
+            Console.CursorVisible= false;
+            string[] words = ReadWordsFromFile();
             while (true)
             {
-                string[] words = ReadWordsFromFile(); 
+                
+                string word = GetRandomWord(words);
+                string wordToGuess = new(Underscore, word.Length);
+                int incorrectGuessCount = 0;
+                List<char> playerUsedLetters = new List<char>();
+                DrawCurrentGameState(false, incorrectGuessCount, wordToGuess, playerUsedLetters);
+                Console.ReadLine();
             }
         }
 
         static string GetRandomWord(string[] words)
         {
-
+            Random random= new Random();
+            string word = words[random.Next(words.Length)];
+            return word.ToLower();
         }
 
-        static void DrawCurrentGameState(bool inputIsInvalid, int incorrectGuess, string guessedWord, List<char> playerUsedLetters)
+        static void DrawCurrentGameState(bool inputIsInvalid, int incorrectGuesses, string guessedWord, List<char> playerUsedLetters)
         {
+            Console.Clear();
+            Console.WriteLine(wrongGuessFrames[incorrectGuesses]);
+            Console.WriteLine($"Guess: {guessedWord}");
+            Console.WriteLine($"You have to guess {guessedWord.Length} symbols.");
+            Console.WriteLine($"The following letters are used: {String.Join(", ", playerUsedLetters)}");
 
+            if (inputIsInvalid )
+            {
+                Console.WriteLine("You should type only a single charecter!")
+            }
+            Console.WriteLine("Your symbol: ");
         }
 
-        static void GamePlay(string word, string wordToGuess, int incorrectGuessCount, List<char> playerUsedLetters)
+        static void PlayGame(string word, string wordToGuess, int incorrectGuessCount, List<char> playerUsedLetters)
         {
-
+            while (true)
+            {
+                string playerInput = Console.ReadLine().ToLower();
+                if (playerInput.Length != 1)
+                {
+                    DrawCurrentGameState(true, incorrectGuessCount, wordToGuess, playerUsedLetters);
+                    continue;
+                }
+                char playerLetter = char.Parse(playerInput);
+                playerUsedLetters.Add(playerLetter);
+            }
         }
 
         static bool CheckIfSymbolIsContained(string word, char playerLetter)
@@ -434,7 +465,6 @@
 
         static string[] ReadWordsFromFile()
         {
-            Console.CursorVisible= false;
             string currentDirectory = Directory.GetCurrentDirectory();
             string projectDirectory = Directory.GetParent(currentDirectory).Parent.Parent.FullName;
             const string WordsFileName = "words.txt";
